cd878567a63a37514b8fcacc51f5ec01ab613346
aburahan8721 <aburaihan8721@gmail.com>
2024-01-01 17:37:23 +0600

    create branch

--- src/components/singlePost/SinglePost.tsx
+++ src/components/singlePost/SinglePost.tsx
@@ -1,62 +1,15 @@
-/*
-import { Link, useLocation } from "react-router-dom";
-import "./singlePost.css";
-import { useEffect, useState } from "react";
-import axios from "axios";
-import { IPost } from "../../../types/index";
-
-export default function SinglePost() {
-  const [post, setPost] = useState<IPost>({} as IPost);
-  const location = useLocation();
-  const path = location.pathname.split("/")[2];
-  const PF = `http://localhost:5000/images/`;
-
-  useEffect(() => {
-    const getPost = async () => {
-      const res = await axios.get(`http://localhost:5000/api/posts/` + path);
-      setPost(res.data);
-    };
-    getPost();
-  }, [path]);
-  return (
-    <div className="singlePost">
-      <div className="singlePostWrapper">
-        {post.photo && <img className="postImg" src={PF + post.photo} alt="" />}
-        <h1 className="singlePostTitle">
-          {post.title}
-          <div className="singlePostEdit">
-            <i className="singlePostIcon far fa-edit"></i>
-            <i className="singlePostIcon far fa-trash-alt"></i>
-          </div>
-        </h1>
-        <div className="singlePostInfo">
-          <span className="singlePostAuthor">
-            Author:
-            <Link to={`/?user=${post.username}`} className="link">
-              <b> {post.username}</b>
-            </Link>
-          </span>
-          <span>{new Date(post.createdAt).toDateString()}</span>
-        </div>
-        <p className="singlePostDesc">{post.desc}</p>
-      </div>
-    </div>
-  );
-}
-
-*/
-
 import axios from "axios";
 import { useContext, useEffect, useState } from "react";
 import { useLocation } from "react-router";
 import { Link } from "react-router-dom";
 import { Context } from "../../context/Context";
 import "./singlePost.css";
+import { IPost } from "../../../types/index";
 
 export default function SinglePost() {
   const location = useLocation();
   const path = location.pathname.split("/")[2];
-  const [post, setPost] = useState({});
+  const [post, setPost] = useState<IPost>({} as IPost);
   const PF = "http://localhost:5000/images/";
   const { user } = useContext(Context);
   const [title, setTitle] = useState("");
@@ -80,7 +33,9 @@ export default function SinglePost() {
         data: { username: user.username },
       });
       window.location.replace("/");
-    } catch (err) {}
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   const handleUpdate = async () => {
@@ -91,7 +46,9 @@ export default function SinglePost() {
         desc,
       });
       setUpdateMode(false);
-    } catch (err) {}
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   return (
--- src/context/Context.tsx
+++ src/context/Context.tsx
@@ -1,15 +1,17 @@
-import { createContext, useEffect, useReducer } from "react";
+import { ReactNode, createContext, useEffect, useReducer } from "react";
 import Reducer from "./Reducer";
 
+const storedUser = localStorage.getItem("user");
+const user = storedUser ? JSON.parse(storedUser) : null;
+
 const INITIAL_STATE = {
-  user: JSON.parse(localStorage.getItem("user")) || null,
-  isFetching: false,
+  user: user,
   error: false,
 };
 
 export const Context = createContext(INITIAL_STATE);
 
-export const ContextProvider = ({ children }) => {
+export const ContextProvider = ({ children }: { children: ReactNode }) => {
   const [state, dispatch] = useReducer(Reducer, INITIAL_STATE);
 
   useEffect(() => {
